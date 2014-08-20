0234683ae304b89c8ee8944185a995f51e516d62
cuevasclemente <cuevasclemente@gmail.com>
2014-08-20 10:10:04 -0400

    Removed Simple Cluster and ThreshClust.jl, both obsolete code lines Removed all references to Debug (still had one dangling)

--- ThreshClustTest.jl
+++ ThreshClustTest.jl
@@ -1,9 +1,12 @@
-using ThreshClust
+using ThreshCluster
 mydistance(a,b) = abs(a-b)
 threshcrit = Simple_Threshold_Criteria(:size,mydistance,4)
 type Fish
 	size
 end
 array_of_fish = [Fish(2),Fish(5),Fish(64),Fish(12)]
+println("array_of_fish\n",array_of_fish)
 
 (cluster_lookup,myarray) = make_simple_threshold_clusters(array_of_fish,threshcrit)
+println(cluster_lookup)
+println(myarray)
