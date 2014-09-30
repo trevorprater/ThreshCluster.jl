af5d903bd80d72c3ab7a5f6d370297c773109a0f
cuevasclemente <cuevasclemente@gmail.com>
2014-09-30 16:27:50 -0400

    Cleaning up and documenting

--- src/ThreshClusterTypes.jl
+++ src/ThreshClusterTypes.jl
@@ -2,25 +2,37 @@
 abstract Cluster
 abstract Cluster_Container
 type Simple_Cluster_Container <: Cluster_Container
-	#This is a simple cluster container. It is an additive type containing an object, and an array of references to which cluster the object belongs to.
+	"A simple container to associate membership information with an object"
 	object
-	compared_quantity #This is the quantity of the object that one desires to measure against
+	compared_quantity #This is the quantity of the object that one desires to measure against, purely for book keeping, as we now use accessor methods
 	membership
 end
 export Simple_Cluster_Container
-#=type Simple_Cluster <: Cluster=#
-	#="""The simple cluster contains a dictionary of cluster profiles (centroids, etc.) indexed to an array of cluster containers. It also contains=#
-#=end=#
-abstract Membership_Criteria                                                                                                                                                                                 
+
+abstract Membership_Criteria
+"Theoretically, we could want other kinds of membership criteria for clustering"
+
 export Membership_Criteria
-abstract Threshold_Criteria <: Membership_Criteria                                                                                                                                                           
+abstract Threshold_Criteria <: Membership_Criteria
+"For Threshold Clustering, but perhaps we could support other criteria in future?"
 export Threshold_Criteria
-type Simple_Threshold_Criteria <: Threshold_Criteria                                                                                                                                                         
-        compared_quantity# The quantity of the object you wish to compare against
-	#This is now purely for illustrative purposes and is not used in the clustering code
+type Simple_Threshold_Criteria <: Threshold_Criteria
+        compared_quantity # The quantity of the object you wish to compare against, purely for book keeping, the accessor method is the only actually important part
 	compared_quantity_accessor::Function
-	"""The change from compared_quantity::Symbol to compared_quantity_accessor makes the API more confusing, but also more robust: it is an almost necessary change in order to do the divisive clustering this library was built for. Now, instead of passing a symbol that points to a type field, instead, this now asks for an accessor function you would call on an object of the type you want to perform the clustering against. That is where the library will get the value it is going to cluster with"""
-	distance_function                                                                                                                                                                                    
-        threshold                                                                                                                                                                                            
-end                                                                                                                                                                                                          
+	"The method applied to an object that returns the quantity you wish to cluster against"
+	"""e.g: if we are clustering cats by their tail length, then compared_quantity_accessor could be: 
+		function get_tail_length(cat::Cat)
+			return cat.tail_length
+		end
+	Or more simply with a lambda:
+	compared_quantity_accessor = cat-> cat.tail_length"""
+	distance_function
+	"A metric that measures two values you obtain via the compared_quantity accessor"
+	"""e.g: if we are clustering cats by their tail length, then distance_function could be:
+		function 2norm(tail_length1, tail_length2)
+			return abs(tail_length1 - tail_length2)
+		end
+	"""
+        threshold
+end
 export Simple_Threshold_Criteria  
